b9b9df193390411cd9db6bb530125e3b2833f920
benevolent-listener-0694 <pranas_29@yahoo.co.uk>
2019-05-29 18:22:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ function logWhisper(string){
       return "YES INDEED!"
      }else if (string === string.toLowerCase()){
           return "I can't hear you!"
-     }
-      else if ( string === "I love you, Grandma."){
+     
+      }else if ( string === "I love you, Grandma."){
             return  "I love you, too"
      
       }
